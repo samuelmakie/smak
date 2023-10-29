0715c63db514ccf614710c11e5eee92dac3be4c7
thesilvercord <87156466+thesilvercord@users.noreply.github.com>
2023-10-29 12:06:34 +0000

    Editing documentation for rbind

--- R/as_annum.R
+++ R/as_annum.R
@@ -9,6 +9,7 @@
 #' @export
 #'
 #' @examples
+#'
 as_annum <- function(date, from = "09-01", sep = " ", format = "%Y"){
   annum_ending <- as.numeric(format(date, format)) + as.integer(date >= as.Date(paste0(format(date, "%Y"), "-", from), format = "%Y-%m-%d"))
   output <- paste0(annum_ending - 1, sep, annum_ending)
--- R/rbind.R
+++ R/rbind.R
@@ -1,24 +1,22 @@
 
 #' rbind
-#'
-#' @param ... Objects of class data.frame
-#' @param col_names Names of the columns of the returned dataframe.
-#' Defaults to NULL, which would create new column names in the form "col_n". It could take a character vector.
-#' Column names could be inherited from a specific dataframe by specifying col_names as an integer indicating which dataframe from those provided by it's order or
-#' by specifying a dataframe object the column names would be inherited from.
-#'
+#' @description A modified `base::rbind`. Binds dataframes by rows according to column order from the left (ignores column names, unlike `base::rbind`) while allowing standarising column names and classes.
+#' @param ... One or more objects of class data.frame to be bound rowwise.
+#' @param col_names  Column names of the returned dataframe.
+#' Defaults to NULL, which creates new column names in the form "col_n".
+#' Can be a character vector, an integer indicating the nth dataframe among those provided from which the column names are inherited, or a dataframe from which the column names are inherited.
 #'
 #' @param col_classes Class of the column vectors in the returned dataframe.
-#' Defaults to NULL, which would create classes naturally.
-#' It could take a character vector of coersion functions (e.g. c(as.character, as.factor, as.Date2)).
-#' Classes could be inherited from a specific dataframe by specifying col_names as an integer indicating which dataframe from those provided by it's order or
-#' by specifying a dataframe object the classes would be inherited from.
-#'
+#' Defaults to NULL, which creates classes naturally.
+#' Can be a vector of coercion functions (e.g. `c(as.character, as.factor, as.Date2))`, an integer indicating the nth dataframe among those provided from which the classes are inherited, or a dataframe from which the classes are inherited.
+#' Note that issues will likely arise when dealing with dates. Try the as.Date2 function if it didn't work as expected, otherwise, user defined functions can be used (e.g. `c(as.character, function(x){as.Date(x, origin = "1970-01-01")})`).
+
 #' @return An object of class data.frame
 #' @export
-#'
+#' @details Can be used for other goals, particularly standardising column names and/or classes among multiple dataframes
 #' @examples
 rbind <- function(..., col_names = NULL, col_classes = NULL){
+
   df_list <- list(...)
 
   col_names <- col_names_f(col_names, df_list)
@@ -29,4 +27,97 @@ rbind <- function(..., col_names = NULL, col_classes = NULL){
     col_classes_f(col_classes = col_classes, df_list = df_list)
 
   return(output)
+
 }
+
+# Helpers ----------------------------------------------------------------------------------------------------
+
+# rbind_col_names
+# Different methods for different inputs for the col_names argument in the rbind function
+ rbind_col_names <- function(col_names, df_list){
+   UseMethod("rbind_col_names")
+ }
+
+ rbind_col_names.NULL <- function(col_names, df_list){
+   paste0("col_", 1:max(unlist(lapply(df_list, FUN = function(x){length(x)}))))
+ }
+
+ rbind_col_names.data.frame <- function(col_names, df_list){
+   c(names(col_names), paste0("col_", 1:max(unlist(lapply(df_list, FUN = function(x){length(x)}))))[-c(1:length(names(col_names)))])
+ }
+
+ rbind_col_names.numeric <- function(col_names, df_list){
+   c(names(df_list[[col_names]]), paste0("col_", 1:max(unlist(lapply(df_list, FUN = function(x){length(x)}))))[-c(1:length(names(df_list[[col_names]])))])
+ }
+
+rbind_col_names.character <- function(col_names, df_list){
+  c(col_names, paste0("col_", 1:max(unlist(lapply(df_list, FUN = function(x){length(x)}))))[-c(1:length(col_names))])
+}
+
+
+# rbind_col_classes
+# Different methods for different inputs for the col_classes argument in the rbind function
+
+rbind_col_classes <- function(col_classes, df_list, output){
+  UseMethod("rbind_col_classes")
+}
+
+rbind_col_classes.NULL <- function(col_classes, df_list, output){
+  return(output)
+}
+
+rbind_col_classes.list <- function(col_classes, df_list, output){
+  output[, c(1:length(col_classes))] <- eval(parse(text = paste0("list(", paste0(paste0("col_classes[[", 1:length(col_classes), "]](output[ ,", 1:length(col_classes), "])"), collapse = ", "), ")")))
+  return(output)
+}
+
+rbind_col_classes.numeric <- function(col_classes, df_list, output){
+  col_classes <- gsub("Date", "Date2", sapply(df_list[[col_classes]], class))
+  output[ ,c(1:length(col_classes))] <- eval(parse(text = paste0("list(", paste0(paste0("as.", col_classes, "(output[, ", 1:length(col_classes), "])"), collapse = ", "), ")")))
+  return(output)
+}
+
+rbind_col_classes.data.frame <- function(col_classes, df_list, output){
+  col_classes <- gsub("Date", "Date2", sapply(col_classes, class))
+  output[ ,c(1:length(col_classes))] <- eval(parse(text = paste0("list(", paste0(paste0("as.", col_classes, "(output[, ", 1:length(col_classes), "])"), collapse = ", "), ")")))
+  return(output)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
